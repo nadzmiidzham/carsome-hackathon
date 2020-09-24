800f5bfa87919f227c5dd16c05a4367fe2910238
nadzmi.idzham <nadzmi.idzham@carsome.com>
2020-09-24 10:09:56 +0800

    refactor

--- src/app/user/user.controller.ts
+++ src/app/user/user.controller.ts
@@ -1,8 +1,5 @@
-import { Body, Controller, Get, Param, Post, Query } from '@nestjs/common';
-import { GetQueryPipe } from 'src/common/pipe/get-query.pipe';
-import { QueryDto } from 'src/shared/dtos/query.dto';
+import { Body, Controller, Get, Param, Post } from '@nestjs/common';
 import { CreateUserDto } from './dtos/create-user.dto';
-import { User } from './entities/user.entity';
 import { UserService } from './user.service';
 
 @Controller('user')
@@ -10,7 +7,7 @@ export class UserController {
   constructor(private readonly userService: UserService) {}
 
   @Get()
-  getAllUsers(@Query() query, @Query(GetQueryPipe) pagination: QueryDto) {
+  getAllUsers() {
     return this.userService.getAll();
   }
 
